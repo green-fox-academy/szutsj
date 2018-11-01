8bef37527857f0f805fce0c163d1a047e48a04ff
Judit Szuts <judit.szuts.mob@gmail.com>
2018-11-01 23:01:22 +0100

    StudentCounter fixed

--- week05/src/StudentCounter.java
+++ week05/src/StudentCounter.java
@@ -9,46 +9,78 @@ public class StudentCounter {
         row0.put("name", "Theodor");
         row0.put("age", 9.5);
         row0.put("candies", 2);
-        map.add( row0 );
+        map.add(row0);
 
         Map<String, Object> row1 = new HashMap<String, Object>();
-        row1.put("name", "Theodor");
-        row1.put("age", 9.5);
-        row1.put("candies", 2);
-        map.add( row1 );
+        row1.put("name", "Paul");
+        row1.put("age", 10);
+        row1.put("candies", 1);
+        map.add(row1);
 
         Map<String, Object> row2 = new HashMap<String, Object>();
-        row2.put("name", "Theodor");
-        row2.put("age", 9.5);
-        row2.put("candies", 2);
-        map.add( row2 );
+        row2.put("name", "Mark");
+        row2.put("age", 7);
+        row2.put("candies", 3);
+        map.add(row2);
 
         Map<String, Object> row3 = new HashMap<String, Object>();
-        row3.put("name", "Theodor");
-        row3.put("age", 9.5);
-        row3.put("candies", 2);
-        map.add( row3 );
+        row3.put("name", "Peter");
+        row3.put("age", 12);
+        row3.put("candies", 5);
+        map.add(row3);
 
         Map<String, Object> row4 = new HashMap<String, Object>();
-        row4.put("name", "Theodor");
-        row4.put("age", 9.5);
-        row4.put("candies", 2);
-        map.add( row4 );
+        row4.put("name", "Olaf");
+        row4.put("age", 12);
+        row4.put("candies", 7);
+        map.add(row4);
 
         Map<String, Object> row5 = new HashMap<String, Object>();
-        row5.put("name", "Theodor");
-        row5.put("age", 9.5);
+        row5.put("name", "George");
+        row5.put("age", 3);
         row5.put("candies", 2);
-        map.add( row5 );
+        map.add(row5);
 
-        System.out.println("Has 4 candies_Ű87hasFourCandies(map));
-        System.out.println(lessThanFiveCandies(map));
+        // Display the following things:
+        //  - Who has got more candies than 4 candies
+        //  - Sum the age of people who have lass than 5 candies
+
+        hasMoreThanFourCandies(map);
+        sumOfAgePossesingLessThanFiveCandies(map);
     }
 
-    public static int hasFourCandies(HashMap<String, Object> map){
-
+    public static void hasMoreThanFourCandies(ArrayList<Map<String, Object>> map) {
+        System.out.printf("The students who have more than four candies: ");
+        Object nulla = 0;
+        Object one = 1;
+        Object two = 2;
+        Object three = 3;
+        Object four = 4;
+        for (int i = 0; i < map.size(); i++) {
+            if (!nulla.equals(map.get(i).get("candies")) && !one.equals(map.get(i).get("candies")) && !two.equals(map.get(i).get("candies")) && !three.equals(map.get(i).get("candies")) && !four.equals(map.get(i).get("candies"))) {
+                System.out.print(map.get(i).get("name") + ", ");
+            }
+        }
+        System.out.println();
     }
 
 
-
+    public static void sumOfAgePossesingLessThanFiveCandies(ArrayList<Map<String, Object>> map) {
+        System.out.print("The age of the students who have less than five candies is : ");
+        int numberOfCandies;
+        double sumOfAgeDouble = 0;
+        int sumOfAgeInt = 0;
+        for (int i = 0; i < map.size(); i++) {
+            numberOfCandies = (int) map.get(i).get("candies");
+            if (numberOfCandies < 5) {
+                if (map.get(i).get("age").getClass().equals(java.lang.Double.class)) {
+                    sumOfAgeDouble += (double) map.get(i).get("age");
+                } else {
+                    sumOfAgeInt += (int) map.get(i).get("age");
+                }
+            }
+        }
+        sumOfAgeDouble += sumOfAgeInt;
+        System.out.println(sumOfAgeDouble);
+    }
 }
