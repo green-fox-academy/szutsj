50ff49221ae1ace6882d5f535e00411daf5ef8ec
Judit Szuts <judit.szuts.mob@gmail.com>
2018-11-06 09:15:22 +0100

    Unique - solved with arraylist

--- week04/day05/src/Unique.java
+++ week04/day05/src/Unique.java
@@ -5,7 +5,30 @@
         //  Returns a list of numbers where every number in the list occurs only once
 
         //  Example
-        //System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+        //
         //  should print: `[1, 11, 34, 52, 61]`
     //}
 //}
+
+// megoldás arraylisttel
+
+import java.util.ArrayList;
+
+public class Unique{
+
+    public static void main(String[] args) {
+        System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+    }
+
+    public static ArrayList<Integer> unique(int[] list){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int index = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            if (!arrayList.contains(list[i])) {
+                arrayList.add(list[i]);
+            }
+        }
+        return arrayList;
+    }
+}
