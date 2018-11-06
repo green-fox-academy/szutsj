88f592467df206937d0dd62f59c8a736d5c6d298
Judit Szuts <judit.szuts.mob@gmail.com>
2018-11-06 09:16:20 +0100

    Palindrome - solved with arraylist

---
+++ week04/day05/src/PalindromeSearcher.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PalindromeSearcher {
+
+    public static void main(String[] args) {
+        Scanner scannerString = new Scanner(System.in);
+        String wordToFindPalindromeIn ="";
+        boolean acceptableWord = true;
+
+        while (acceptableWord) {
+            System.out.println("Please, give me a word (at least 3 characters long!): ");
+            wordToFindPalindromeIn = scannerString.nextLine();
+            if (wordToFindPalindromeIn.length() < 3) {
+                acceptableWord = true;
+            } else {
+                acceptableWord = false;
+            }
+        }
+
+        System.out.println("The list of palindromes in the word are: ");
+        System.out.println(palindrome(wordToFindPalindromeIn));
+    }
+
+
+    public static ArrayList<String> palindrome(String wordToFindPalindromeIn) {
+        char[] letters = wordToFindPalindromeIn.toCharArray();
+        ArrayList<String> palindromes = new ArrayList<>();
+
+        for (int palindromeLength = 3; palindromeLength <= letters.length; palindromeLength++) {
+            for (int indexInArrayList = 0; indexInArrayList < letters.length; indexInArrayList++) {
+
+                if (!endOfString(letters.length, palindromeLength, indexInArrayList) && reallyPalindrome(wordToFindPalindromeIn.substring(indexInArrayList, palindromeLength + indexInArrayList))) {
+                    palindromes.add(wordToFindPalindromeIn.substring(indexInArrayList, palindromeLength + indexInArrayList));
+                }
+            }
+        }
+
+
+        return palindromes;
+    }
+
+    public static boolean endOfString(int lettersLength, int palindromeLength, int index){
+        boolean ended = false;
+        if (lettersLength <= palindromeLength + index){
+            ended = true;
+        }
+
+        return ended;
+    }
+
+    public static boolean reallyPalindrome(String subString) {
+        char[] subStringAsCharArray = subString.toCharArray();
+        boolean reallyP = true;
+
+        for (int j = 0; j < subStringAsCharArray.length; j++) {
+            if (subStringAsCharArray[j] != subStringAsCharArray[subStringAsCharArray.length - 1 - j]) {
+            reallyP = false;
+            }
+        }
+        return reallyP;
+    }
+}
