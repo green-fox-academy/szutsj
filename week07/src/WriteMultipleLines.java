178156c1556865729d80f472362f29485082b8d7
Judit Szuts <judit.szuts.mob@gmail.com>
2018-11-15 18:09:32 +0100

    WriteMultipleLines solved

---
+++ week07/src/WriteMultipleLines.java
@@ -0,0 +1,41 @@
+// Create a function that takes 3 parameters: a path, a word and a number,
+// than it should write to a file.
+// The path parameter should be a string, that describes the location of the file.
+// The word parameter should be a string, that will be written to the file as lines
+// The number paramter should describe how many lines the file should have.
+// So if the word is "apple" and the number is 5, than it should write 5 lines
+// to the file and each line should be "apple"
+// The function should not raise any error if it could not write the file.
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WriteMultipleLines {
+
+  public static void main(String[] args) {
+    writingGivenNumberOfLines("./src/my-file3.txt", "I have just made what you asked from me.", 5);
+  }
+
+  public static void writingGivenNumberOfLines(String filePath, String text, int numberOfLines){
+    List<String> lines = new ArrayList<>();
+    ArrayList<String> textToWrite = new ArrayList<>(Arrays.asList(text));
+
+    try{
+      Path thePath = Paths.get(filePath);
+
+      for (int i = 0; i < numberOfLines; i++) {
+        Files.write(thePath, textToWrite, StandardOpenOption.APPEND);
+      }
+      lines = Files.readAllLines(thePath);
+      System.out.println(lines);
+    } catch (IOException e){
+      System.out.println("Unable to write the file.");
+    }
+  }
+}
