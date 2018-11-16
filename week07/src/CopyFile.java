cc0389045355341b331b28eb57c5e18320a1a33b
Judit Szuts <judit.szuts.mob@gmail.com>
2018-11-16 21:49:23 +0100

    CopyFile.java changed

--- week07/src/CopyFile.java
+++ week07/src/CopyFile.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
 
 public class CopyFile {
 
@@ -21,7 +20,6 @@ public class CopyFile {
       Path secondPath = Paths.get(filename2);
       Files.copy(firstPath, secondPath);
     } catch (IOException e){
-      System.out.println("There is a problem with one of the files");
       return false;
     }
     return true;
