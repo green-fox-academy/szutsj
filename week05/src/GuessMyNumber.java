30ce2d119264f8513b307cba7bde7881870acd19
Judit Szuts <judit.szuts.mob@gmail.com>
2018-11-16 21:48:39 +0100

    GuessMyNumber fixed

--- week05/src/GuessMyNumber.java
+++ week05/src/GuessMyNumber.java
@@ -1,5 +1,5 @@
 //Exercise
- //       Write a program where the program chooses a number between 1 and 100.
+//       Write a program where the program chooses a number between 1 and 100.
 //  The player is then asked to enter a guess.
 // If the player guesses wrong, then the program gives feedback and
 // ask to enter an other guess until the guess is correct.
@@ -19,30 +19,30 @@ import java.util.Scanner;
 
 public class GuessMyNumber {
 
-    public static void main(String[] args) {
-        System.out.println("Please enter the first number of the range: ");
-        Scanner scannerInt = new Scanner(System.in);
-        int rangeMin = scannerInt.nextInt();
-        System.out.println("Please enter the first number of the range: ");
-        int rangeMax = scannerInt.nextInt();
-        System.out.println("How many lives do you want: ");
-        int lives = scannerInt.nextInt();
-        GuessMyNumber(rangeMin, rangeMax, lives);
-    }
+  public static void main(String[] args) {
+    System.out.println("Please enter the first number of the range: ");
+    Scanner scannerInt = new Scanner(System.in);
+    int rangeMin = scannerInt.nextInt();
+    System.out.println("Please enter the first number of the range: ");
+    int rangeMax = scannerInt.nextInt();
+    System.out.println("How many lives do you want: ");
+    int lives = scannerInt.nextInt();
+    GuessMyNumber(rangeMin, rangeMax, lives);
+  }
 
-    public static void  GuessMyNumber(int rangeMin, int rangeMax, int lives){
-           int randomNumber = (int)(rangeMin + Math.random() * (rangeMax -  rangeMin));
-        for (int i = lives ; i > 0 ; i--) {
-            System.out.println("Please give me your tipp: ");
-            Scanner scannerInt = new Scanner(System.in);
-            int guess = scannerInt.nextInt();
-            if (guess == randomNumber){
-                System.out.println("You won!");
-                break;
-            }
-            System.out.println("You have " + (i - 1) + " lives left.");
-        }
+  public static void GuessMyNumber(int rangeMin, int rangeMax, int lives) {
+    int randomNumber = rangeMin + (int) (Math.random() * (rangeMax - rangeMin));
 
-        }
+    for (int i = lives; i > 0; i--) {
+      System.out.println("Please give me your tipp: ");
+      Scanner scannerInt = new Scanner(System.in);
+      int guess = scannerInt.nextInt();
+      if (guess == randomNumber) {
+        System.out.println("You won!");
+        break;
+      }
+      System.out.println("You have " + (i - 1) + " lives left.");
     }
+  }
+}
 
