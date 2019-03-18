709cfc51b20c3babf7268dc17b6074af511a8473
Szuts Judit <judit.szuts.mob@gmail.com>
2019-03-18 21:15:22 +0100

    Todo app with database with MariaDb - delete method implemented

--- week22/todowithdatabase2/src/main/java/com/greenfoxacademy/todowithdatabase2/controller/TodoController.java
+++ week22/todowithdatabase2/src/main/java/com/greenfoxacademy/todowithdatabase2/controller/TodoController.java
@@ -6,10 +6,7 @@ import com.greenfoxacademy.todowithdatabase2.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,6 +46,12 @@ public class TodoController {
   @PostMapping("add")
   public String addNewTodo(@RequestParam String title){
     todoRepository.save(new Todo(title));
+    return "redirect:/todo/list";
+  }
+
+  @GetMapping("{id}/delete")
+  public String delete(@PathVariable Long id){
+    todoRepository.deleteById(id);
     return "redirect:/todo/list";
   }
 
