52eb6bafe1a30a43b066999daea589cc3dcb6957
Szuts Judit <judit.szuts.mob@gmail.com>
2019-04-01 13:18:20 +0200

    RestExercise : greeter and error messages implemented

--- week24/rest1/src/main/java/com/greenfoxacademy/restfirst/controller/OtherControllers.java
+++ week24/rest1/src/main/java/com/greenfoxacademy/restfirst/controller/OtherControllers.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.restfirst.controller;
 
 import com.greenfoxacademy.restfirst.model.Doubling;
 import com.greenfoxacademy.restfirst.model.Error;
+import com.greenfoxacademy.restfirst.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,23 @@ public class OtherControllers {
       return new Error("Please provide an input!");
     }
     return new Doubling(input);
+  }
+
+  @GetMapping("/greeter")
+  public Object greeting(@RequestParam (required = false) String name, String title){
+    if (name == null){
+      if (title == null){
+        return new Error("Please provide a name and title!");
+      } else {
+        return new Error("Please provide a name!");
+      }
+    }
+
+    if (title == null) {
+      return new Error("Please provide a title!");
+    }
+
+    return new Greeting(name, title);
   }
 
 }
