bac43b1f4dcf3631302e62d5e5c904db73fe2ae5
Szuts Judit <judit.szuts.mob@gmail.com>
2019-04-03 18:12:19 +0200

    RestExercise : translate endpoint fixed

--- week24/rest1/src/main/java/com/greenfoxacademy/restfirst/model/TextToTranslate.java
+++ week24/rest1/src/main/java/com/greenfoxacademy/restfirst/model/TextToTranslate.java
@@ -3,14 +3,12 @@ package com.greenfoxacademy.restfirst.model;
 public class TextToTranslate {
 
   private String text;
-  private String lang;
 
   public TextToTranslate() {
   }
 
   public TextToTranslate(String text) {
     this.text = text;
-    this.lang = hu;
   }
 
   public String getText() {
